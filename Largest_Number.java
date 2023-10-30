7aab7e68f3b7308b8c7c3f0e2e8fe5fbd6dc01e9
pavnsai <pavansai18.a@gmail.com>
2023-10-29 23:03:07 -0600

    Day 6

---
+++ Largest_Number.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class SolutionLargestNumber {
+    public String largestNumber(int[] nums) {
+        String [] arr= new String[nums.length];
+        for(int i=0;i<nums.length;i++){
+            arr[i]=nums[i]+"";
+        }
+        Arrays.sort(arr,(a, b)-> {
+            String s1 = a + "" + b;
+            String s2 = b + "" + b;
+            return s2.compareTo(s1);
+        });
+        String ans="";
+        for(int i=0;i<nums.length;i++){
+            ans+=arr[i];
+        }
+        if(ans.charAt(0)=='0'){
+            return "0";
+        }
+        return ans;
+    }
+}
