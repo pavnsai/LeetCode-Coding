ed0ab6ab9efa73ae1cd3d615262c7437dcc4a69e
pavnsai <pavansai18.a@gmail.com>
2023-12-27 22:08:10 -0700

    Day 16

--- LongestSubstringWithoutRepeatingCharacters.java
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -13,4 +13,20 @@ public class LongestSubstringWithoutRepeatingCharacters {
         }
         return ans;
     }
+
+    // Second Approach
+    public int lengthOfLongestSubstring2(String s) {
+        int ans=0,l=0,r=0,n=s.length();
+        int freq[]=new int[256];
+        while(r<n){
+            freq[s.charAt(r)]++;
+            while(freq[s.charAt(r)]>1){
+                freq[s.charAt(l)]--;
+                l++;
+            }
+            ans=Math.max(ans,r-l+1);
+            r++;
+        }
+        return ans;
+    }
 }
