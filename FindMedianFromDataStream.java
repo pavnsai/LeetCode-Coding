92a8915d7be05f0a59664b540df009a8d2339111
pavnsai <pavansai18.a@gmail.com>
2024-01-07 12:09:34 -0700

    Day 24

--- FindMedianFromDataStream.java
+++ FindMedianFromDataStream.java
@@ -33,3 +33,39 @@ public class FindMedianFromDataStream {
         }
     }
 }
+
+// Can use Binary Search for searching as it is sorted list
+class FindMedianFromDataStreamBinarySearch {
+    List<Integer> list;
+    public FindMedianFromDataStreamBinarySearch() {
+        list=new ArrayList<Integer>();
+    }
+    public int binarySearch(int low,int high,int num){
+        if(low<=high){
+            int mid=low+(high-low)/2;
+            int val=list.get(mid);
+            if(val==num){
+                return mid;
+            }
+            else if(val>num){
+                return binarySearch(low,mid-1,num);
+            }else{
+                return binarySearch(mid+1,high,num);
+            }
+        }
+        return high+1;
+    }
+    public void addNum(int num) {
+        int idx=binarySearch(0,list.size()-1,num);
+        list.add(idx,num);
+    }
+
+    public double findMedian() {
+        int middle=list.size()/2;
+        if(list.size()%2==0){
+            return (double)(list.get(middle)+list.get(middle-1))/2;
+        }else{
+            return list.get(middle);
+        }
+    }
+}
