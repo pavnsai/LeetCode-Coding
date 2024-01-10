6400773acdf4be624d3c8a9a33b8ffc00883bdae
pavnsai <pavansai18.a@gmail.com>
2024-01-10 14:02:21 -0700

    Day 27

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -12,3 +12,18 @@ public class ClimbingStairs {
         return arr[n];
     }
 }
+// Without using space complexity
+class ClimbingStairsNoSpace {
+    public int climbStairs(int n) {
+        if(n==1||n==2){
+            return n;
+        }
+        int a=1,b=2,c=0;
+        for(int i=3;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+}
