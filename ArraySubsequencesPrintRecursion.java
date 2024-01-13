74c2606a1a74faff1cd1ab866042e1f30361eaea
pavnsai <pavansai18.a@gmail.com>
2024-01-13 14:28:45 -0700

    Day 28

--- ArraySubsequencesPrintRecursion.java
+++ ArraySubsequencesPrintRecursion.java
@@ -69,10 +69,31 @@ public class ArraySubsequencesPrintRecursion {
             return true;
         return false;
     }
+    static int printNumberOfSubsequencesofSumKOptimized(int idx, ArrayList<Integer> al,int arr[], int n,int sum,int currentSum){
+        if(idx==n){
+            if(currentSum==sum) {
+                //condition satisfied
+                for (Integer num : al) {
+                    System.out.print(num + " ");
+                }
+                System.out.println("");
+                return 1;
+            }
+            return 0;
+        }
+        al.add(arr[idx]);
+        int l=printNumberOfSubsequencesofSumKOptimized(idx+1,al,arr,n,sum,currentSum+arr[idx]);
+
+        al.remove(al.size()-1);
+        int r=printNumberOfSubsequencesofSumKOptimized(idx+1,al,arr,n,sum,currentSum);
+
+        return l+r;
+    }
 
     public static void main(String[] args) {
         int arr[]={1,2,1};
 //        printSubsequences(0,new ArrayList<Integer>(),arr,3);
-        printOneSubsequencesofSumKOptimized(0,new ArrayList<Integer>(),arr,3,2,0);
+//        printOneSubsequencesofSumKOptimized(0,new ArrayList<Integer>(),arr,3,2,0);
+        System.out.println(printNumberOfSubsequencesofSumKOptimized(0,new ArrayList<Integer>(),arr,3,4,0));
     }
 }
