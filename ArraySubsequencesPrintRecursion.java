c374ddb7f7f9a4231e701abc2625d158944352b0
pavnsai <pavansai18.a@gmail.com>
2024-01-13 14:18:17 -0700

    Day 28

--- ArraySubsequencesPrintRecursion.java
+++ ArraySubsequencesPrintRecursion.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class ArraySubsequencesPrintRecursion {
-    static void print(int idx, ArrayList<Integer> al,int arr[], int n){
+    static void printSubsequences(int idx, ArrayList<Integer> al,int arr[], int n){
         if(idx==n){
             for(Integer num:al){
                 System.out.print(num+" ");
@@ -10,13 +10,69 @@ public class ArraySubsequencesPrintRecursion {
             return;
         }
         al.add(arr[idx]);
-        print(idx+1,al,arr,n);
-        al.remove(Integer.valueOf(arr[idx]));
-        print(idx+1,al,arr,n);
+        printSubsequences(idx+1,al,arr,n);
+        al.remove(al.size()-1);
+        printSubsequences(idx+1,al,arr,n);
+    }
+
+    static void printSubsequencesofSumK(int idx, ArrayList<Integer> al,int arr[], int n,int sum,int currentSum){
+        if(idx==n){
+            if(currentSum==sum) {
+                for (Integer num : al) {
+                    System.out.print(num + " ");
+                }
+                System.out.println("");
+            }
+            return;
+        }
+        al.add(arr[idx]);
+        printSubsequencesofSumK(idx+1,al,arr,n,sum,currentSum+arr[idx]);
+        al.remove(al.size()-1);
+        printSubsequencesofSumK(idx+1,al,arr,n,sum,currentSum);
+    }
+    static boolean flag=true;
+    static void printOneSubsequencesofSumK(int idx, ArrayList<Integer> al,int arr[], int n,int sum,int currentSum){
+        if(idx==n){
+            if(flag&&(currentSum==sum)) {
+                flag=false;
+                for (Integer num : al) {
+                    System.out.print(num + " ");
+                }
+                System.out.println("");
+
+            }
+            return;
+        }
+        al.add(arr[idx]);
+        printOneSubsequencesofSumK(idx+1,al,arr,n,sum,currentSum+arr[idx]);
+        al.remove(al.size()-1);
+        printOneSubsequencesofSumK(idx+1,al,arr,n,sum,currentSum);
+    }
+
+    static boolean printOneSubsequencesofSumKOptimized(int idx, ArrayList<Integer> al,int arr[], int n,int sum,int currentSum){
+        if(idx==n){
+            if(currentSum==sum) {
+                //condition satisfied
+                for (Integer num : al) {
+                    System.out.print(num + " ");
+                }
+                System.out.println("");
+                return true;
+            }
+            return false;
+        }
+        al.add(arr[idx]);
+        if(printOneSubsequencesofSumKOptimized(idx+1,al,arr,n,sum,currentSum+arr[idx]))
+            return true;
+        al.remove(al.size()-1);
+        if(printOneSubsequencesofSumKOptimized(idx+1,al,arr,n,sum,currentSum))
+            return true;
+        return false;
     }
 
     public static void main(String[] args) {
-        int arr[]={3,1,2};
-        print(0,new ArrayList<Integer>(),arr,3);
+        int arr[]={1,2,1};
+//        printSubsequences(0,new ArrayList<Integer>(),arr,3);
+        printOneSubsequencesofSumKOptimized(0,new ArrayList<Integer>(),arr,3,2,0);
     }
 }
