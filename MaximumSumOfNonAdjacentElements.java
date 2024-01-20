e0bab2ec76a7df3313cff8564ea7a6bd79635171
pavnsai <pavansai18.a@gmail.com>
2024-01-20 12:02:03 -0700

    Day 34

--- MaximumSumOfNonAdjacentElements.java
+++ MaximumSumOfNonAdjacentElements.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+
 //TLE 1
  class MaximumSumOfNonAdjacentElementsTLE1 {
     public static void sum(int idx, ArrayList<Integer> nums,
@@ -34,4 +36,38 @@ import java.util.ArrayList;
          int ans[]=new int[nums.size()];
          return sum(0,nums,nums.size(),arr);
      }
+}
+//Optimized
+class MaximumSumOfNonAdjacentElementsMemoization {
+
+    public static int sum(int idx, ArrayList<Integer> nums,
+                          int n, int ans[]) {
+        if (idx >= n) {
+            return 0;
+        }
+        int pick = nums.get(idx);
+        if (ans[idx + 2] == -1) {
+            pick += sum(idx + 2, nums, n, ans);
+        } else {
+            pick += ans[idx + 2];
+        }
+        int notPick = 0;
+        if (ans[idx + 1] == -1) {
+            notPick += sum(idx + 1, nums, n, ans);
+        } else {
+            notPick += ans[idx + 1];
+        }
+        ans[idx] = Math.max(pick, notPick);
+        return ans[idx];
+    }
+
+    public static int maximumNonAdjacentSum(ArrayList<Integer> nums) {
+        // Write your code here.
+        int arr[] = {Integer.MIN_VALUE};
+        int ans[] = new int[nums.size() + 2];
+        Arrays.fill(ans, -1);
+        ans[nums.size()] = 0;
+        ans[nums.size() + 1] = 0;
+        return sum(0, nums, nums.size(), ans);
+    }
 }
