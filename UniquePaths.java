6889d6ee79f01003a1dba44b4ae9f091e14167f7
pavnsai <pavansai18.a@gmail.com>
2024-01-23 12:43:57 -0700

    Day 37

--- UniquePaths.java
+++ UniquePaths.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class UniquePathsTLE {
     public void dfs(int a,int b,int m,int n,boolean visited[][],int arr[]){
         if(a==m-1&&b==n-1){
@@ -36,4 +38,40 @@ class UniquePathsAnotherTLE {
     public int uniquePaths(int m, int n) {
         return dfs(m-1,n-1);
     }
+}
+
+class UniquePathsAnotherMemoization {
+    public int dfs(int a,int b,int ans[][]){
+        if(a==0&&b==0){
+            return 1;
+        }
+        if(a<0||b<0){
+            return 0;
+        }
+        int up=0,left=0;
+        if(a-1>=0&&ans[a-1][b]==-1){
+            up=dfs(a-1,b,ans);
+        }else if(a-1>=0){
+            up=ans[a-1][b];
+        }else{
+            up=0;
+        }
+        if(b-1>=0&&ans[a][b-1]==-1){
+            left=dfs(a,b-1,ans);
+        }else if(b-1>=0){
+            left=ans[a][b-1];
+        }else{
+            left=0;
+        }
+        ans[a][b]=up+left;
+        return ans[a][b];
+    }
+    public int uniquePaths(int m, int n) {
+        int ans[][]=new int [m][n];
+        for(int i=0;i<m;i++){
+            Arrays.fill(ans[i],-1);
+        }
+        return dfs(m-1,n-1,ans);
+    }
+
 }
