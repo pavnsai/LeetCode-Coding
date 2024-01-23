ba68339e6d4407acea7c65ef6cfb3e7912546121
pavnsai <pavansai18.a@gmail.com>
2024-01-23 13:10:10 -0700

    Day 38

--- UniquePaths.java
+++ UniquePaths.java
@@ -101,3 +101,32 @@ class UniquePathsAnotherMemoization2 {
         return dfs(m - 1, n - 1, ans);
     }
 }
+class UniquePathsDP {
+    public int uniquePaths(int m, int n) {
+        int ans[][] = new int[m][n];
+        for(int i=0;i<m;i++){
+            ans[i][0]=1;
+        }
+        for(int i=0;i<n;i++){
+            ans[0][i]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                // int l=0,r=0;
+                // if(i-1<0){
+                //     l=0;
+                // }else{
+                //     l=ans[i-1][j];
+                // }
+                // if(j-1<0){
+                //     r=0;
+                // }else{
+                //     r=ans[i][j-1];
+                // }
+                // ans[i][j]=l+r;
+                ans[i][j]=ans[i-1][j]+ans[i][j-1];
+            }
+        }
+        return ans[m-1][n-1];
+    }
+}
