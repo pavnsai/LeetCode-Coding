e63897d132645f265e09aeae40438a9efeba098b
pavnsai <pavansai18.a@gmail.com>
2024-02-13 11:37:14 -0700

    Day 51

--- BestTimeToBuyAndSellStockIV.java
+++ BestTimeToBuyAndSellStockIV.java
@@ -95,3 +95,73 @@ class BestTimeToBuyAndSellStockIVDP2 {
         return forw[1][0];
     }
 }
+
+//Memoization2
+class BestTimeToBuyAndSellStockIVMemoization2 {
+    public int check(int i, int trans,int k,int prices[],int ans[][]){
+        if(trans==2*k||i==prices.length){
+            return 0;
+        }
+        if(ans[i][trans]!=-1){
+            return ans[i][trans];
+        }
+        int profit=0;
+        if(trans%2==0){
+            profit=Math.max(-prices[i]+check(i+1,trans+1,k,prices,ans),check(i+1,trans,k,prices,ans));
+        }else{
+            profit=Math.max(prices[i]+check(i+1,trans+1,k,prices,ans),check(i+1,trans,k,prices,ans));
+        }
+        return ans[i][trans]=profit;
+    }
+    public int maxProfit(int k, int[] prices) {
+        int n=prices.length;
+        int ans[][]=new int[n][2*k];
+        for(int i=0;i<n;i++){
+            Arrays.fill(ans[i],-1);
+        }
+        return check(0,0,k,prices,ans);
+    }
+}
+
+//DP3
+class BestTimeToBuyAndSellStockIVDP3 {
+    public int maxProfit(int k, int[] prices) {
+        int n=prices.length;
+        int ans[][]=new int[n+1][2*k+1];
+        for(int i=n-1;i>=0;i--){
+            for(int trans=2*k-1;trans>=0;trans--){
+                int profit=0;
+                if(trans%2==0){
+                    profit=Math.max(-prices[i]+ans[i+1][trans+1],ans[i+1][trans]);
+                }else{
+                    profit=Math.max(prices[i]+ans[i+1][trans+1],ans[i+1][trans]);
+                }
+                ans[i][trans]=profit;
+            }
+        }
+        return ans[0][0];
+    }
+}
+
+//DP4
+class BestTimeToBuyAndSellStockIVDP4 {
+    public int maxProfit(int k, int[] prices) {
+        int n=prices.length;
+        int curr[]=new int[2*k+1];
+        int forw[]=new int[2*k+1];
+        for(int i=n-1;i>=0;i--){
+            for(int trans=2*k-1;trans>=0;trans--){
+                int profit=0;
+                if(trans%2==0){
+                    profit=Math.max(-prices[i]+forw[trans+1],forw[trans]);
+                }else{
+                    profit=Math.max(prices[i]+forw[trans+1],forw[trans]);
+                }
+                curr[trans]=profit;
+            }
+            forw=Arrays.copyOf(curr,curr.length);
+        }
+        return forw[0];
+    }
+
+}
