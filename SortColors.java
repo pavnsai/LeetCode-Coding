269c28728d04203c52289264545a5b08f6da8f82
pavnsai <pavansai18.a@gmail.com>
2024-02-15 20:42:01 -0700

    Day 52

---
+++ SortColors.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class SortColors {
+    public void swap(int[] nums, int a,int b){
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
+    }
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+        int l=0,r=nums.length-1,m=0;
+        while(m<r){
+            if(nums[m]==0){
+                swap(nums,m,l);
+                l++;
+                m++;
+            }else if(nums[m]==2){
+                swap(nums,m,r);
+                r--;
+            }else{
+                m++;
+            }
+        }
+    }
+}
