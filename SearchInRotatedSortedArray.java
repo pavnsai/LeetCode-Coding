1783fbcd53e044d2d2ce35dd872a7ff1a76a3ef9
pavnsai <pavansai18.a@gmail.com>
2024-02-17 16:06:45 -0700

    Day 53

---
+++ SearchInRotatedSortedArray.java
@@ -0,0 +1,25 @@
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int l=0,r=nums.length-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(nums[m]==target){
+                return m;
+            }
+            if(nums[m]<nums[r]){
+                if(target<=nums[r]&&target>nums[m]){
+                    l=m+1;
+                }else{
+                    r=m-1;
+                }
+            }else{
+                if(target<nums[m]&&nums[l]<=target){
+                    r=m-1;
+                }else{
+                    l=m+1;
+                }
+            }
+        }
+        return -1;
+    }
+}
